33a1d0ed46be010cdbb5bb045f4524f56af2a929
mikaeltreppo <mikaeltreppo@outlook.com>
2023-05-30 23:25:42 +0200

    fine progetto con aggiunta di if in cards

---
+++ src/components/MyCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card m-3" v-for="proj in projects" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ proj.title }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{ proj.author }}</h6>
+            <p class="card-text">{{ proj.description }}</p>
+
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    name: 'MyCard',
+    data() {
+        return {
+            projects: [],
+        }
+    },
+    methods: {
+        getCard() {
+            axios.get("http://127.0.0.1:8000/api/projects").then(response => {
+                this.projects = response.data.results;
+            });
+        }
+    },
+    mounted() {
+        if(this.projects.length==0){
+        this.getCard()};
+    }
+}
+
+
+</script>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container d-flex flex-wrap justify-content-center">
+
+        <!--cards-->
+        <MyCard></MyCard>
+
+    </div>
+</template>
+<script>
+
+import MyCard from './MyCard.vue';
+
+export default {
+    components: { MyCard },
+    name: "MyMain",
+    data() {
+        return {};
+    }
+  
+
+}
+</script>
